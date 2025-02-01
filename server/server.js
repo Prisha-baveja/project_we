86322afb99485387d57026469123975468128bd4
Prisha-baveja <prishabaveja76@gmail.com>
2025-02-01 19:26:56 +0530

    Initial commit 1

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ import channelRoutes from "./routes/ChannelRoutes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 
 app.use(cors({
   origin:[process.env.ORIGIN], 
